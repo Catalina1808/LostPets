d41672fbc8779c4327183a58c75c9ceb73b200e7
Catalina <catalina.g1808@gmail.com>
2021-06-08 23:41:31 +0300

    Added a new fragment

--- app/src/main/java/com/example/lostpet/fragments/FragmentAdd.java
+++ app/src/main/java/com/example/lostpet/fragments/FragmentAdd.java
@@ -1,11 +1,20 @@
 package com.example.lostpet.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentAdd extends Fragment {
+import com.example.lostpet.R;
+import com.example.lostpet.interfaces.OnFragmentActivityCommunication;
 
+public class FragmentAdd extends Fragment {
+    private OnFragmentActivityCommunication activityCommunication;
     public static final String TAG_FRAGMENT_ADD = "TAG_FRAGMENT_ADD";
 
     public static Fragment newInstance() {
@@ -15,4 +24,38 @@ public class FragmentAdd extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if(context instanceof OnFragmentActivityCommunication){
+            activityCommunication= (OnFragmentActivityCommunication) context;
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+/*
+        view.findViewById(R.id.btn_register).setOnClickListener(v -> {
+            goToRegister();
+        });
+
+ */
+
+    }
+    /*
+    private  void goToRegister(){
+        if(activityCommunication !=null){
+            activityCommunication.onReplaceFragment(FragmentRegister.TAG_FRAGMENT_REGISTER);
+        }
+    }
+
+     */
 }
