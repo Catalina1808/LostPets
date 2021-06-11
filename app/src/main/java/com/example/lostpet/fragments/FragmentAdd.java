c34a471dc244e549252210dcd484e26a90b88eb8
Catalina <catalina.g1808@gmail.com>
2021-06-11 14:26:59 +0300

    Made some modifications on the interface

--- app/src/main/java/com/example/lostpet/fragments/FragmentAdd.java
+++ app/src/main/java/com/example/lostpet/fragments/FragmentAdd.java
@@ -1,10 +1,14 @@
 package com.example.lostpet.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +17,15 @@ import androidx.fragment.app.Fragment;
 import com.example.lostpet.R;
 import com.example.lostpet.interfaces.OnFragmentActivityCommunication;
 
+import static android.app.Activity.RESULT_OK;
+
 public class FragmentAdd extends Fragment {
     private OnFragmentActivityCommunication activityCommunication;
     public static final String TAG_FRAGMENT_ADD = "TAG_FRAGMENT_ADD";
+    private Button BSelectImage;
+    private ImageView IVPreviewImage;
+    private int SELECT_PICTURE = 200;
+
 
     public static Fragment newInstance() {
         Bundle args = new Bundle();
@@ -46,8 +56,16 @@ public class FragmentAdd extends Fragment {
         view.findViewById(R.id.btn_register).setOnClickListener(v -> {
             goToRegister();
         });
-
  */
+        BSelectImage = view.findViewById(R.id.BSelectImage);
+        IVPreviewImage = view.findViewById(R.id.IVPreviewImage);
+
+        BSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+        });
 
     }
     /*
@@ -56,6 +74,36 @@ public class FragmentAdd extends Fragment {
             activityCommunication.onReplaceFragment(FragmentRegister.TAG_FRAGMENT_REGISTER);
         }
     }
-
      */
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    IVPreviewImage.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
 }
