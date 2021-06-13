ac7014e4cf33a34e911fb8ba227dbd761b6721a2
Catalina <catalina.g1808@gmail.com>
2021-06-13 16:11:06 +0300

    Added the possibility to sign out

--- app/src/main/java/com/example/lostpet/fragments/FragmentMain.java
+++ app/src/main/java/com/example/lostpet/fragments/FragmentMain.java
@@ -1,21 +1,31 @@
 package com.example.lostpet.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.lostpet.R;
+import com.example.lostpet.activities.RandomPictureActivity;
+import com.example.lostpet.activities.WelcomeActivity;
 import com.example.lostpet.interfaces.OnFragmentActivityCommunication;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class FragmentMain extends Fragment {
     public static final String TAG_FRAGMENT_MAIN="TAG_FRAGMENT_MAIN";
     private OnFragmentActivityCommunication activityCommunication;
+    private ProgressBar progressBar;
 
     public static FragmentMain newInstance() {
 
@@ -43,6 +53,7 @@ public class FragmentMain extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        progressBar = view.findViewById(R.id.pb_loading_main);
 
         view.findViewById(R.id.btn_add).setOnClickListener(v -> {
             goToAdd();
@@ -55,6 +66,10 @@ public class FragmentMain extends Fragment {
 
         view.findViewById(R.id.btn_view_mine).setOnClickListener(v -> {
             goToViewMine();
+        });
+
+        view.findViewById(R.id.btn_sing_out).setOnClickListener(v -> {
+            signOut();
         });
 
 
@@ -79,5 +94,20 @@ public class FragmentMain extends Fragment {
             activityCommunication.onAddFragment(FragmentViewMine.TAG_FRAGMENT_VIEW_MINE);
         }
     }
+
+    private void signOut() {
+        progressBar.setVisibility(View.VISIBLE);
+        FirebaseAuth mAuth=FirebaseAuth.getInstance();
+        mAuth.signOut();
+       Toast.makeText(getContext(), "Signed out.",
+                        Toast.LENGTH_SHORT).show();
+        progressBar.setVisibility(View.GONE);
+
+        startActivity(new Intent(getActivity(), WelcomeActivity.class));
+        Objects.requireNonNull(getActivity()).finish();
+    }
+
+
+
 
 }
